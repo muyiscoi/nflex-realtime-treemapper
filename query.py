a82b3ef28b3ca6006bf5cce6a7b5d70b213c3995
Georgi Dimitrov <georgijd92@gmail.com>
2016-10-19 12:09:48 +0100

    Add the stub for querying the metrics API

--- query.py
+++ query.py
@@ -1,1 +1,20 @@
-"""Poll stuff"""
+from datetime import datetime, timedelta
+
+
+def get_metrics(metrics, context):
+    end = datetime.utcnow()
+    start = end - timedelta(minutes=5)
+    resource_id = "2d16ab4e-68c5-449b-9608-7c67c570dac1"
+    metric = "cpu-usage"
+    params = {
+        "start": fmt_date(start),
+        "end": fmt_date(end),
+        "downsampling": "max",
+    }
+    url = '/metrics/%s/resources/%s/points' % (metric, resource_id)
+    resp = context.api.get(url, params=params)
+    return resp.json()
+
+
+def fmt_date(date):
+    return datetime.strftime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
