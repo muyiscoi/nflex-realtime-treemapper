31cfc08c02aaba16a232eae755a23b5bb8cbb87a
Georgi Dimitrov <georgijd92@gmail.com>
2016-10-19 14:37:01 +0100

    Get data based on a rule

--- main.py
+++ main.py
@@ -1,2 +1,49 @@
+from datetime import datetime, timedelta
+from query import get_resources_for_application, get_metrics
+import operator
+import re
+
+
+RULE = {
+    "application": "CMP (Core)",
+    "rule": {
+        "type": "spike",
+        "metric": "cpu-usage",
+        "value": 10,
+        "child_metric": "docker-cpu-usage.*",
+    }
+}
+
+
 def get(event, context):
-    print "call stuff"
+    end = datetime.utcnow()
+    start = end - timedelta(minutes=20)
+
+    name = RULE.get("application")
+    name = re.sub(r'([()])', r'\\\1', name)
+    resources = get_resources_for_application(context, name)
+    rule = RULE["rule"]
+    data = {}
+    for resource in resources:
+        data[resource['id']] = r = {}
+        r['name'] = resource['base']['name']
+        if rule["type"] == "spike":
+            metric = rule['metric']
+            metrics = get_metrics(context,
+                                  resource['id'],
+                                  metric,
+                                  start,
+                                  end)
+            r['metrics'] = metrics
+            points = metrics[0]['values'][-2:]
+            values = map(operator.itemgetter('value'),
+                         points)
+            if (values[1] - values[0]) > rule['value']:
+                child_metrics = get_metrics(context,
+                                            resource['id'],
+                                            rule['child_metric'],
+                                            start,
+                                            end)
+                r['child_metrics'] = child_metrics
+
+    return data
--- query.py
+++ query.py
@@ -10,26 +10,27 @@ def get_metrics(context,
     params = {
         "start": fmt_date(start),
         "end": fmt_date(end),
-        "downsampling": "max",
+        "downsampling": downsampling,
     }
     url = '/metrics/%s/resources/%s/points' % (metric, resource_id)
     resp = context.api.get(url, params=params)
     return resp.json()
 
+
 def fmt_date(date):
     return datetime.strftime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
 
-"""
-    Pass in an id for an application to get the list of
-    resources that are part of this application
-"""
-def get_resources_for_application(context, id):
-    url = '/query';
 
+def get_resources_for_application(context, name):
+    """
+        Pass in an id for an application to get the list of
+        resources that are part of this application
+    """
+    url = '/query'
     json = {
         "queries": [{
-                "key": "connections.applications.id",
-                "values": [ id ]
+                "key": "connections.applications.name",
+                "values": [name],
             }
         ],
         "metadata_queries": [],
@@ -39,18 +40,21 @@ def get_resources_for_application(context, id):
         ],
         "group_by_attributes": [],
         "aggregations": []
-    };
+    }
 
     response = context.api.post(url, json)
-    #print(response.json())
     return response.json()["items"]
 
-def get_metrics_for_application(context, id, metric, start, end):
 
-    resources = get_resources_for_application(context, id)
+def get_metrics_for_application(context, name, metric, start, end):
+    resources = get_resources_for_application(context, name)
     resource_metrics = []
     for resource in resources:
-        resource_metric = get_metrics(context, resource['id'], metric, start, end)
+        resource_metric = get_metrics(context,
+                                      resource['id'],
+                                      metric,
+                                      start,
+                                      end)
         resource_metrics.append(resource_metric)
 
     return resource_metrics
