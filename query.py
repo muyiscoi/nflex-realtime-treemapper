d7e3e00be34993d545ba53e1f4ed16dba6ccf963
Glen Pike <glen.pike@headforwards.com>
2016-10-19 12:33:42 +0100

    add query for application resources

--- query.py
+++ query.py
@@ -15,6 +15,31 @@ def get_metrics(metrics, context):
     resp = context.api.get(url, params=params)
     return resp.json()
 
-
 def fmt_date(date):
     return datetime.strftime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+"""
+    Pass in an id for an application to get the list of
+    resources that are part of this application
+"""
+def get_resources_for_application(id):
+    url = '/query';
+
+    json = {
+        "queries": [{
+                "key": "connections.applications.id",
+                "values": [ id ]
+            }
+        ],
+        "metadata_queries": [],
+        "attributes": [
+            "id",
+            "base.name"
+        ],
+        "group_by_attributes": [],
+        "aggregations": []
+    };
+
+    response = context.api.post(url, json)
+    #print(response.json())
+    return response.json()["items"]
